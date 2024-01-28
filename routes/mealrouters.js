2103e76c04a6b04cd375f8a623b896373761fe3b
ewi josepha <ewijosepha@gmail.com>
2024-01-28 16:36:31 +0100

    adding post

--- routes/mealrouters.js
+++ routes/mealrouters.js
@@ -7,13 +7,14 @@ let router = express.Router()
 // var model = require('../model')
 
 router.get('/', function (req, res, next) {
-  const query = 
-  `select  mealId, mealName, strArea,mealstrYoutube, mealInstructions, mealMeasurements, mealcategory.categoryName, mealIngredients.ingredients  from recipe  join mealcategory on
-  recipe.mealCatId = mealcategory.categoryId join mealingredients;
-  `
+  const query =
+    `select  meals.mealId, mealName, mealArea,mealYoutube, mealUrl, mealctegory.category, mealinstructions.instruction, mealinstructions.step from meals  join mealctegory on
+  meals.categoryId = mealctegory.categoryId
+  join mealinstructions on meals.mealId= mealinstructions.mealId
+  ;`
   console.log(res.statusCode)
   connection.query(query, (err, data) => {
-    if (err) { 
+    if (err) {
       console.error(err)
       if (err.message === "not found") next()
       else next()
@@ -26,16 +27,18 @@ router.get('/', function (req, res, next) {
 
 router.get('/:id', function (req, res, next) {
   const id = req.params.id
-  const query2 = `select * from recipe where mealId = ${id};`
+  const query2 = `select  meals.mealId, mealName, mealArea,mealYoutube, mealUrl, mealctegory.category, mealinstructions.instruction, mealinstructions.step from meals  join mealctegory on
+  meals.categoryId = mealctegory.categoryId
+  join mealinstructions on meals.mealId= mealinstructions.mealId where meals.mealId = ${id};`
   connection.query(query2, (err, data) => {
     if (err) {
       next(err)
     } else {
-      if ((data.length = 0)) {
-        let error = new Error(`recipe with mealId ${id} not found`)
-        err.status = 404
-        res.send(err)
-        next(err)
+      if ((data.length === 0)) {
+        let error = new Error(`mealproject with mealId ${id} not found`)
+        error.status = 404;
+        res.status(404).send(error);
+        next(error);
       } else {
         res.send(data[0])
 
@@ -45,11 +48,19 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-  const { mealId,mealName, strArea,mealstrYoutube,mealInstructions,mealmeasurements } = req.body;
-  console.log(req.body);
-  const creationQuery = `insert into recipe(   mealId, mealName, strArea,mealstrYoutube, mealInstructions, mealMeasurements, mealcategory.categoryName, mealIngredients.ingredients  from recipe  right join mealcategory on
-  recipe.mealCatId = mealcategory.categoryId right join mealingredients on recipe.mealId) values ("${8}", "${eru}", "${Nigeria}", "${httphdhhdjjgdfees}", "${cookLeaves}","${pansofEru}", "${vegetables}","${cryfish}",)`;
-  connection.query(creationQuery, (err, data) => {
+  // const { categoryId, category } = req.body
+  const categoryquerry = `insert into mealcategory(categoryId, category)  values ("2", "${Dessert}")`
+
+  const mealquerry = `insert into meals(mealId, mealName, mealArea, mealUrl,mealYoutube, categoryId)  values ("2", "${sandwish}", "${USA}", "${"https://images.unsplash.com/photo-1555554317-766200eb80d6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2FuZHdpc2h8ZW58MHx8MHx8fDA%3D"}", "${null}, "${2}");`
+
+  connection.query(mealquerry, (err, data) => {
+    if (err) next(err)
+    else res.status(201).send(data)
+  })
+
+  const instructionsquerry = `insert into mealInstructions(instructionId, mealId,stepCount, instruction, step)  values ("2", "${2}", "${2}", "${'[{"instruction": "follow the steps carefully"}]'}", "${'[{"step1": "SLICE tomatoes and other ingredients"},{"step2": "bake your flour"}, {"step3": "spread spices into the bread"}]'}" )
+  ;`
+  connection.query(instructionsquerry, (err, data) => {
     if (err) next(err)
     else res.status(201).send(data)
   })
@@ -58,14 +69,14 @@ router.post('/', function (req, res, next) {
 router.delete("/:id", function (req, res, next) {
   const id = req.params.id
   const deletedquery = `delete from recipe where id = ${id}`
-  connection.query(deletedquery, (err, data)=>{
+  connection.query(deletedquery, (err, data) => {
     if (err) {
       console.log(err);
       if (err.message === "not found") next()
       else {
         next()
       }
-    }else{
+    } else {
       res.send(data)
     }
   })
